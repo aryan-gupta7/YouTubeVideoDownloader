277f5898c262b3ffec9a9ea6db8397952128328e
Aryan Gupta <71920399+aryan-gupta7@users.noreply.github.com>
2021-06-04 23:18:40 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# YouTube Video Downloader
+### Prerequisites
+* Installed Python3.6 or higher
+### Libraries required
+* PyTube -  install with `pip install pytube`
+* tqdm - install with `pip install tqdm`
+
+Or you can simply download all required libraries by `pip install -r requirements.txt`
+
+_After installing, run the `main.py` file and provide the YouTube video link and the path to save video as the inputs._
---
+++ main.py
@@ -0,0 +1,58 @@
+# Importing Required Packages.
+import os
+import pytube
+import subprocess
+from tqdm import tqdm
+
+
+print("Hey there...\nThis is a python program developed by Aryan Gupta to download youtube video through links.")
+
+# Getting Video link through input/
+video_link = input("Enter the link for the video to download - ")
+
+# Checking if link is INVALID
+try:
+    youtube = pytube.YouTube(video_link)
+except:
+    if len(video_link) < 35:
+        print("Invalid Link provided")
+        quit()
+    else:
+        print("Connect to Internet first!")
+        quit()
+
+# Getting the save path
+save_path = input("Enter the save path (format - 'disk:/folder/name' e.g. 'E:/yt_downloads') - ") 
+# Checking if the path is valid.
+try:
+    os.chdir(save_path)
+except:
+    print("Wrong path provided!")
+    quit()
+
+# Getting the first video of all the streams
+video = youtube.streams.first()
+title = video.title
+
+# Creating a loading bar.
+for i in tqdm(range(1),desc=f'Downloading {title[:20]}...'):
+    try:
+        # Downloading video to the save path
+        video.download(save_path) 
+    except: 
+        print("Some Error!") 
+
+# Getting the Filename.
+files_in_dir = os.listdir(save_path)
+for i in files_in_dir:
+    if not i.endswith(".mp4"):
+        files_in_dir.pop(files_in_dir.index(i))
+filename = ''
+for video in files_in_dir:
+    if video[:5] == title[:5]:
+        filename = video
+
+# Opening the file explorer with selected downloaded video.
+filepath = os.path.abspath(filename)
+print(f'Video downloaded to {filepath}')
+subprocess.Popen(f'explorer /select, "{filepath}"')
